8f5cdea08b98a90aebbba04802d09b9868d14106
ztandrews <68918006+ztandrews@users.noreply.github.com>
2023-12-26 15:47:04 -0500

    V 1.0.1
    - Got scraper more prod ready
    - Cleaned file structure
    - Removed logging
    - Changed examples file from ipynb to script

---
+++ examples.py
@@ -0,0 +1,30 @@
+################### examples.py ###################
+#                                                 #
+#            Quick program to provide             #
+#            Examples of the scraper in use       #
+#                                                 #
+###################################################
+
+
+# Import modules
+from scraper import nhl_pbp_data_scraper as nhlpbpds
+import pandas
+
+# Scrape a single game
+game_id = 2023020350
+single_game_pbp = nhlpbpds.scrape_game(game_id)
+print(single_game_pbp.head())
+
+# Scrape all games from a given date
+date = "2023-12-20"
+date_pbp = nhlpbpds.scrape_date(date)
+print(date_pbp.head())
+
+# If you prefer to have the data in a csv, you can use pd.to_csv to save each df after scraping, or just directly call it like this
+nhlpbpds.scrape_game(game_id).to_csv("game_pbp.csv")
+
+# If you're interested in doing some analysis to a df, go wild. Here are some basic examples for beginners
+
+# All goals from a single game
+goals = single_game_pbp.loc[single_game_pbp['event']=='goal']
+print(goals[['event','event_team','event_primary_player','description']])
--- scraper/nhl_pbp_data_scraper.py
+++ scraper/nhl_pbp_data_scraper.py
@@ -1,7 +1,8 @@
-######################################### nhl_pbp_data_scraper.py ###########################################
+######################################### nhl_pbp_data_scraper.py ######################################
 #                                                                                                      #
 #                                      Name: NHL PBP Data Scraper                                      #
-#                                         V: 1.0.0                                                     #
+#                                         V: 1.0.1                                                     #
+#                                V. Updates: Removed logging, cleaner folder structure                 #
 #                                       Dev: Zach Andrews                                              #
 #                                     About: This is a Python program to scrape and clean NHL          #
 #                                            play-by-play data. The project is built by Zach           #
@@ -13,57 +14,50 @@
 import pandas as pd
 import requests
 import numpy as np
-import logging
 from bs4 import BeautifulSoup as bs
 ############################################# Config ###################################################
-logging.basicConfig(filename='scraper.log',level=logging.INFO, format='%(asctime)s - %(levelname)s  - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
-logging.info("live_scraper.py program started")
 
 # Function to scrape games from a certain date
 def scrape_date(date):
     print("Scraping games from {}...\n".format(date))
-    logging.info("scrape_date function called. Scraping games from {}...".format(date))
     # Make request to days schedule. Example URL: https://api-web.nhle.com/v1/schedule/2023-11-30
     try:
         req = requests.get("https://api-web.nhle.com/v1/schedule/{}".format(date))
-        logging.info("Schedule API request status: {}".format(req))
         req.raise_for_status() 
     # Handle any exception related to the request
     except requests.exceptions.RequestException as req_exc:
-        logging.error(f"Play-by-Play API request failed: {req_exc}")
+        print(f"Play-by-Play API request failed: {req_exc}")
     # Handle HTTP errors
     except requests.exceptions.HTTPError as http_err:
-        logging.error(f"Play-by-Play API HTTP error occurred: {http_err}")
+        print(f"Play-by-Play API HTTP error occurred: {http_err}")
     # Handle value-related issues
     except ValueError as val_err:
-        logging.error(f"Play-by-Play API Value error occured: {val_err}")
+        print(f"Play-by-Play API Value error occured: {val_err}")
     else:
         games = pd.json_normalize(req.json()['gameWeek'][0]['games'])['id'].tolist()
         pbp_df = pd.DataFrame()
         for game in games:
             game_pbp = scrape_game(game)
             pbp_df = pd.concat([pbp_df,game_pbp])
-    logging.info("scrape_date function finished.")
+    print("{} Finished.".format(date))
     return pbp_df
 
 # Function to scrape single game
 def scrape_game(game_id):
     print("Scraping game {}...".format(game_id))
-    logging.info(" ********************* scrape_game function called. Scraping game {}... *********************".format(game_id))
     # Make request. New URL example is https://api-web.nhle.com/v1/gamecenter/2023020061/play-by-play
     try:
-        req = requests.get("https://api-web.nhle.com/v1/gamecenter/{}/play-by-play".format(game_id))
-        logging.info("Play-by-Play API request status: {}".format(req))
+        req = requests.get("https://api-web.nhle.com/v1/gamecenter/{}/play-by-play".format(game_id))  
         req.raise_for_status() 
     # Handle any exception related to the request
     except requests.exceptions.RequestException as req_exc:
-        logging.error(f"Play-by-Play API request failed: {req_exc}")
+        print(f"Play-by-Play API request failed: {req_exc}")
     # Handle HTTP errors
     except requests.exceptions.HTTPError as http_err:
-        logging.error(f"Play-by-Play API HTTP error occurred: {http_err}")
+        print(f"Play-by-Play API HTTP error occurred: {http_err}")
     # Handle value-related issues
     except ValueError as val_err:
-        logging.error(f"Play-by-Play API Value error occured: {val_err}")
+        print(f"Play-by-Play API Value error occured: {val_err}")
     else:
          # Get PBP and transform into df
         # Full df is the entire json resp. includes some general game info
@@ -81,26 +75,23 @@ def scrape_game(game_id):
         players_df = clean_players(players_df,teams)
         # Clean pbp
         pbp = clean_pbp(pbp,players_df,teams)
-        logging.info("********************* scrape_game function finished for game {}. *********************".format(game_id))
         print("Game {} finished.\n".format(game_id))
     return pbp
 
 # Add in lots of misc info that we need to get from not just the pbp frame. Coaches, etc
 def add_misc_info(pbp,full_df,game_id):
-    logging.info("add_misc_info function called...")
     #For tons more of misc info not on the regualr pbp endpoint go to https://api-web.nhle.com/v1/gamecenter/2022030237/landing
     try:
         req = requests.get("https://api-web.nhle.com/v1/gamecenter/{}/landing".format(game_id))
-        logging.info("Gamecenter API request status: {}".format(req))
         req.raise_for_status() 
     except requests.exceptions.RequestException as req_exc:
-        logging.error(f"Gamecenter API request failed: {req_exc}")
+        print(f"Gamecenter API request failed: {req_exc}")
     # Handle HTTP errors
     except requests.exceptions.HTTPError as http_err:
-        logging.error(f"Gamecenter API HTTP error occurred: {http_err}")
+        print(f"Gamecenter API HTTP error occurred: {http_err}")
     # Handle value-related issues
     except ValueError as val_err:
-        logging.error(f"Gamecenter API Value error occured: {val_err}")
+        print(f"Gamecenter API Value error occured: {val_err}")
     else:
         gamecenter = req.json()
         pbp['home_team'] = gamecenter['homeTeam']['abbrev']
@@ -112,12 +103,10 @@ def add_misc_info(pbp,full_df,game_id):
         pbp['home_coach']  = gamecenter['summary']['gameInfo']['homeTeam']['headCoach']['default'].upper()
         pbp['away_coach'] = gamecenter['summary']['gameInfo']['awayTeam']['headCoach']['default'].upper()
         pbp['game_id'] = game_id
-        logging.info("add_misc_info function finished.")
     return pbp
 
 # Function to get home and away team
 def get_teams(full_df):
-    logging.info("get_teams function called...")
     teams = {}
     home_id = full_df.iloc[0]['homeTeam.id']
     home_abv = full_df.iloc[0]['homeTeam.abbrev']
@@ -125,12 +114,10 @@ def get_teams(full_df):
     away_abv = full_df.iloc[0]['awayTeam.abbrev']
     teams[home_id]=home_abv
     teams[away_id]=away_abv
-    logging.info("get_teams function finished.")
     return teams
 
 # Function to clean the pbp df that will return
 def clean_pbp(pbp_df,players_df,teams):
-    logging.info("clean_pbp function called...")
     # Add p1 id, p2 id, p3 mid
     pbp_df = add_event_players(pbp_df,players_df)
     # Add teams
@@ -147,22 +134,18 @@ def clean_pbp(pbp_df,players_df,teams):
     pbp_df = add_shootout_logic(pbp_df)
     #Clean columns
     pbp_df = clean_columns(pbp_df)
-    logging.info("clean_pbp function finished.")
     return pbp_df
 
 # Function to clean the players df to be used throughout the scraper
 def clean_players(players_df,teams):
-    logging.info("clean_players function called...")
     players_df['team'] = players_df['teamId'].map(teams)
     players_df['player'] = players_df['firstName.default'].str.upper() + " " + players_df['lastName.default'].str.upper()
     players_df = players_df[['player',"playerId","team","teamId","sweaterNumber","positionCode"]]
     players_df = players_df.rename(columns={"playerId":"player_id","teamId":"team_id","sweaterNumber":"sweater_number","positionCode":"position"})
-    logging.info("clean_players function finished.")
     return players_df
 
 # Function to add players to the event action columns
 def add_event_players(pbp_df,players_df):
-    logging.info("add_event_players function called...")
     #Goals
     #frst, check if columns exist. There is no guarentee a non-so goal will be scored, and if thats the case
     # we will not have the assistPlayerId columns. So we have to check for that
@@ -193,12 +176,10 @@ def add_event_players(pbp_df,players_df):
     pbp_df['event_primary_player'] = pbp_df['event_primary_id'].map(games_players)
     pbp_df['event_secondary_player'] = pbp_df['event_secondary_id'].map(games_players)
     pbp_df['event_tertiary_player'] = pbp_df['event_tertiary_id'].map(games_players)
-    logging.info("add_event_players function finished.")
     return pbp_df
 
 # Function to check if all detail columns are present
 def check_columns(pbp_df):
-    logging.info("check_columns function called...")
     cols = pbp_df.columns.tolist()
     needed = ['details.assist1PlayerId','details.assist2PlayerId','details.committedByPlayerId','details.drawnByPlayerId','details.servedByPlayerId',
               'details.blockingPlayerId','details.hittingPlayerId','details.hitteePlayerId','details.shootingPlayerId','details.scoringPlayerId',
@@ -208,24 +189,20 @@ def check_columns(pbp_df):
     # Convert the result back to a list
     result = list(difference)
     pbp_df[result] = np.nan
-    logging.info("check_columns function finished.")
     return pbp_df
 
 # Function to add event team to df
 def add_event_team(pbp_df,teams,players_df):
-    logging.info("add_event_team function called...")
     # The pbp data gives the event owner ID to the blocker rather than the shooter for blocked shots. We need to change that
     players_dict = dict(zip(players_df['player_id'], players_df['team_id']))
     pbp_df['shootingPlayerTeam_temp'] = pbp_df['details.shootingPlayerId'].map(players_dict)
     pbp_df['details.eventOwnerTeamId'] = np.where(pbp_df['typeDescKey']=="blocked-shot",pbp_df['shootingPlayerTeam_temp'],pbp_df['details.eventOwnerTeamId'])
     # Now map
     pbp_df['event_team'] = pbp_df['details.eventOwnerTeamId'].map(teams)
-    logging.info("add_event_team  function finished.")
     return pbp_df
 
 # Function to add total goals at the time of each event
 def add_total_goals(pbp_df):
-    logging.info("add_total_goals function called...")
     pbp_df['isHomeGoal']=0
     pbp_df['isAwayGoal']=0
     pbp_df.loc[((pbp_df['typeDescKey']=="goal")&(pbp_df['event_team']==pbp_df['home_team'])),"isHomeGoal"] = 1
@@ -234,12 +211,10 @@ def add_total_goals(pbp_df):
     pbp_df['home_score'] = pbp_df['isHomeGoal'].cumsum()
     pbp_df.loc[((pbp_df['typeDescKey']=="goal")&(pbp_df['event_team']==pbp_df['home_team'])),'home_score'] = pbp_df['home_score']-1
     pbp_df.loc[((pbp_df['typeDescKey']=="goal")&(pbp_df['event_team']==pbp_df['away_team'])),'away_score'] = pbp_df['away_score']-1
-    logging.info("add_total_goals function finished.")
     return pbp_df
 
 # Function to parse situation code
 def parse_situation_code(pbp_df):
-    logging.info("parse_situation_code function called...")
     # The situation code is a 4 digit number formatted as follows:
     # AG,AS,HS,HG
     # AG - Away gaolie on ice (1 yes, 0 no)
@@ -272,38 +247,33 @@ def parse_situation_code(pbp_df):
     pbp_df.loc[(pbp_df['typeDescKey']=='shot-on-goal')|(pbp_df['typeDescKey']=="missed-shot")|(pbp_df['typeDescKey']=="blocked-shot")|(pbp_df['typeDescKey']=="goal"),"is_shot_on_empty_net_rel"] = 0
     pbp_df.loc[((pbp_df['situationCode'].str[0]=="0")&(pbp_df['event_team']==pbp_df['home_team'])&((pbp_df['typeDescKey']=='shot-on-goal')|(pbp_df['typeDescKey']=="missed-shot")|(pbp_df['typeDescKey']=="blocked-shot")|(pbp_df['typeDescKey']=="goal"))),"is_shot_on_empty_net_rel"] = 1
     pbp_df.loc[((pbp_df['situationCode'].str[3]=="0")&(pbp_df['event_team']==pbp_df['away_team'])&((pbp_df['typeDescKey']=='shot-on-goal')|(pbp_df['typeDescKey']=="missed-shot")|(pbp_df['typeDescKey']=="blocked-shot")|(pbp_df['typeDescKey']=="goal"))),"is_shot_on_empty_net_rel"] = 1
-    logging.info("parse_situation_code function finished.")
     return pbp_df
 
 # Function to get elapsed time from the timeInPeriod field
 def add_elapsed_time(pbp_df):
-    logging.info("add_elapsed_time function called...")
     pbp_df['period_seconds_elapsed'] = pbp_df['timeInPeriod'].str[:2].astype(int)*60 +  pbp_df['timeInPeriod'].str[3:].astype(int)
     pbp_df['period_minutes_elapsed'] = pbp_df['period_seconds_elapsed']/60
     pbp_df['game_seconds_elapsed'] = np.where(pbp_df['period']==1,pbp_df['period_seconds_elapsed'],(pbp_df['period_seconds_elapsed']+((pbp_df['period']-1)*1200)))
     pbp_df['game_minutes_elapsed'] = pbp_df['game_seconds_elapsed'] /60
-    logging.info("add_elapsed_time function finished.")
     return pbp_df
 
 # Function to add the HTML report to the df. The HTML report is very important as it's the easiest
 # way to get each teams players on ice for each event
 def add_html_report_data(pbp_df, players_df):
-    logging.info("add_html_report function called...")
     # URL example: https://www.nhl.com/scores/htmlreports/20232024/PL010035.HTM
     season = pbp_df.iloc[0]['season']
     trimmed_game_id = str(pbp_df.iloc[0]['game_id'])[5:]
     try:
         req = requests.get("https://www.nhl.com/scores/htmlreports/{}/PL0{}.HTM".format(season,trimmed_game_id))
-        logging.info("HTML Report request status: {}".format(req))
         req.raise_for_status() 
     except requests.exceptions.RequestException as req_exc:
-        logging.error(f"HTML Report request failed: {req_exc}")
+        print(f"HTML Report request failed: {req_exc}")
     # Handle HTTP errors
     except requests.exceptions.HTTPError as http_err:
-        logging.error(f"HTML Report HTTP error occurred: {http_err}")
+        print(f"HTML Report HTTP error occurred: {http_err}")
     # Handle value-related issues
     except ValueError as val_err:
-        logging.error(f"HTML Report Value error occured: {val_err}")
+        print(f"HTML Report Value error occured: {val_err}")
     else:
         # adding this here. The api recogized failed so attempts as their own event where as the html pbp does not. Making this simple change
         pbp_df.loc[pbp_df['typeDescKey']=="failed-shot-attempt",'typeDescKey'] = "missed-shot"
@@ -336,7 +306,7 @@ def add_html_report_data(pbp_df, players_df):
                     away_players = row[6].select("font")
                     home_players = row[7].select("font")
                 except ValueError:
-                    logging.error("Error assigning a variable from an html report row")
+                    print("Error assigning a variable from an html report row")
                 # Clean time because its formatted with elapsed/remaining combined. We will only use elapsed
                 period_time_elapsed = clean_time(time)
                 # Get the event owener from html report
@@ -369,7 +339,6 @@ def add_html_report_data(pbp_df, players_df):
         pbp_df = pbp_df.drop(columns=["period_y",'strength_y','timeInPeriod_y', 'period_minutes_elapsed_y',  'game_minutes_elapsed_y', 'period_seconds_elapsed_y'])
         pbp_df = pbp_df.rename(columns={"period_x":'period','strength_x':"strength",'timeInPeriod_x':"timeInPeriod",'period_minutes_elapsed_x':'period_minutes_elapsed',
                                         'game_minutes_elapsed_x':'game_minutes_elapsed','period_seconds_elapsed_x':'period_seconds_elapsed'})
-    logging.info("add_html_report function finished.")
     return pbp_df
 
 # Func to clean the time that is on the html report
@@ -397,7 +366,6 @@ def add_players(event_dict,players_array,home_or_away):
 # so we need this function to change shootout goals to a new event called shooutout-goals, and then calculate the new score.
 # This will be skipped if there was no shootout in the game
 def add_shootout_logic(pbp_df):
-    logging.info("add_shootout_logic function called...")
     if "shootout-complete" in pbp_df['event'].value_counts().keys().tolist():
         # first, we have to adjust the score to not add up when a shootout goal is scored
         score_end_of_reg = pbp_df[(pbp_df['event']=="period-end")&(pbp_df['period']==4)]['home_score'].tolist()[0]
@@ -422,7 +390,6 @@ def add_shootout_logic(pbp_df):
             pbp_df.loc[pbp_df['event']=='game-end','away_score'] = score_end_of_reg+1
     else:
         pbp_df = pbp_df
-    logging.info("add_shootout_logic function finished.")
     return pbp_df
 
 
@@ -471,7 +438,6 @@ def extract_event_primary_player(event,description,players_df):
 
 # Function to clean the columns 
 def clean_columns(pbp_df):
-    logging.info("clean_columns function called...")
     pbp_df = pbp_df.rename(columns={"situationCode":"situation_code","homeTeamDefendingSide":"home_team_def_side",
                                     "periodDescriptor.periodType":"period_type","details.xCoord":"x_coordinate",
                                     "details.yCoord":"y_coordinate","details.zoneCode":"zone","details.shotType":"shot_type",
@@ -484,5 +450,4 @@ def clean_columns(pbp_df):
                      'home_skaters_on_ice','away_skaters_on_ice','home_goalie_on_ice','away_goalie_on_ice','strength','strength_rel',
                      'strength_cat_rel','home_skater1', 'home_skater2','home_skater3', 'home_skater4', 'home_skater5','home_skater6',
                      'home_goalie','away_skater1', 'away_skater2', 'away_skater3', 'away_skater4','away_skater5','away_skater6','away_goalie']]
-    logging.info("clean_columns function finished.")
     return pbp_df
